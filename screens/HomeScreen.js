8b4d957fe01f58db503d2672432d07048f63eaa2
Deshan Dissanayake <113162275+deshanbdissanayake@users.noreply.github.com>
2024-07-23 13:17:25 +0530

    20240723-1317

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,7 +7,6 @@ import MiniButton from '../components/general/MiniButton'
 import { Entypo } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
 import { DrawerActions } from '@react-navigation/native';
-import { marginRight10 } from '../assets/commonStyles'
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -17,13 +16,6 @@ const HomeScreen = () => {
     navigation.dispatch(DrawerActions.openDrawer());
   }
 
-  const hanldeNewQuote = () => {
-    console.log('new quote');
-  }
-
-  const hanldeAddToFav = () => {
-    console.log('fav');
-  }
 
   if(loading){
     return <LoadingScreen />
@@ -40,21 +32,6 @@ const HomeScreen = () => {
         />
       </View>
       <RuleSlider />
-      <View style={styles.favBtnWrapper}>
-        <MiniButton
-          func={hanldeNewQuote}
-          content={<Entypo name="plus" size={24} color={colors.textColorPri} />}
-          bgColor={colors.bgColorPri}
-          btnStyles={true}
-          wrapperStyles={[marginRight10]}
-        />
-        <MiniButton
-          func={hanldeAddToFav}
-          content={<Entypo name="heart" size={24} color={colors.textColorPri} />}
-          bgColor={colors.bgColorPri}
-          btnStyles={true}
-        />
-      </View>
     </View>
   )
 }
@@ -64,20 +41,13 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15,
+    padding: 20,
     backgroundColor: colors.bgColorPri,
   },
   menuBtnWrapper:{
     position: 'absolute',
-    top: 15,
-    left: 15,
-    zIndex: 1,
-  },
-  favBtnWrapper:{
-    position: 'absolute',
-    flexDirection: 'row',
-    bottom: 15,
-    right: 15,
+    top: 20,
+    left: 20,
     zIndex: 1,
   },
 })
